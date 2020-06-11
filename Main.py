6219c331a5b2b5c912b52429843be7ef02cb536b
YerimYerim <vigvang_bip@naver.com>
2020-06-12 04:11:12 +0900

    콤보박스 add

--- Main.py
+++ Main.py
@@ -1,44 +1,45 @@
-import tkinter
-import tkinter.ttk
+from tkinter import *
 
 # ﻿GUI창을 생성하고 라벨을 설정한다.
-root=tkinter.Tk()
-root.title("Students")
-root.geometry("540x300+100+100")
+from tkinter import ttk
+
+root = Tk()
+root.title("경기 지역화폐가맹점 검색 어플리케이션")
+root.geometry("800x800+100+100")
 root.resizable(False, False)
 
-lbl = tkinter.Label(root, text="Class Notice")
-lbl.pack()
-
 # ﻿표 생성하기. colums는 컬럼 이름, displaycolums는 실행될 때 보여지는 순서다.
-treeview=tkinter.ttk.Treeview(root, columns=["one", "two","three"], displaycolumns=["one","two","three"])
-treeview.pack()
+treeview = ttk.Treeview(root, columns=["one", "two", "three"], displaycolumns=["one", "two", "three"])
+treeview.place(x=0, y=600)
+# 콤보박스 부분 구현
+combo = ttk.Combobox(root, width=20, textvariable=str)
+combo['values'] = (
+    '수원시', '용인시', '성남시', '부천시', '화성시', '안산시', '안양시', '평택시', '시흥시', '김포시', '광주시', '광명시', '군포시'
+    , '하남시', '오산시', '이천시', '안성시', '의왕시', '양평군', '여주시', '과천시', '고양시', '남양주시', '파주시', '의정부시', '양주시'
+    , '구리시', '포천시', '동두천시', '가평군', '연천군'
+)
+combo.place(x=0, y=575)
+combo.current(0)
 
 # 각 컬럼 설정. 컬럼 이름, 컬럼 넓이, 정렬 등
-treeview.column("#0", width=100,)
-treeview.heading("#0", text="index")
+treeview.column("#0", width=50, )
+treeview.heading("#0", text="번호", anchor="center")
 
-treeview.column("#1", width=100, anchor="center")
-treeview.heading("one", text="name", anchor="center")
+treeview.column("#1", width=200, anchor="center")
+treeview.heading("one", text="가게이름", anchor="center")
 
-treeview.column("#2", width=100, anchor="center")
-treeview.heading("two", text="score", anchor="center")
+treeview.column("#2", width=300, anchor="center")
+treeview.heading("two", text="주소", anchor="center")
 
-treeview.column("#3", width=70, anchor="center")
-treeview.heading("three", text="rank", anchor="center")
+treeview.column("#3", width=200, anchor="center")
+treeview.heading("three", text="전화번호", anchor="center")
 
 # 표에 삽입될 데이터
-treelist=[("Tom", 80, 3), ("Bani", 71, 5), ("Boni", 90, 2), ("Dannel", 78, 4), ("Minho", 93, 1)]
+treelist = [("Tom", 80, 3), ("Bani", 71, 5), ("Boni", 90, 2), ("Dannel", 78, 4), ("Minho", 93, 1)]
 
 # 표에 데이터 삽입
 for i in range(len(treelist)):
-    treeview.insert('', 'end', text=i, values=treelist[i], iid=str(i)+"번")
-
-# 슴겨진 항목
-top=treeview.insert('', 'end', text="hidden index", iid="5번")
-top_mid1=treeview.insert(top, 'end', text="5", values=["Timy", 0, 8], iid="5번-1")
-top_mid2=treeview.insert(top, 0, text="6", values=["Ann", 35, 7], iid="5번-0")
-top_mid3=treeview.insert(top, 'end', text="7", values=["Sany", 60, 6], iid="5번-2")
+    treeview.insert('', 'end', text=i, values=treelist[i], iid=str(i) + "번")
 
 # GUI 실행
 root.mainloop()
