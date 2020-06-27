2927956f76409bb070387368bf2d1149ca9a5e5c
YerimYerim <vigvang_bip@naver.com>
2020-06-28 01:50:31 +0900

    add SceneManager.py

--- introScene.py
+++ introScene.py
@@ -2,14 +2,16 @@ from tkinter import *
 
 
 class introScene:
-    def __init__(self,root):
-
+    def __init__(self, root):
         self.image = PhotoImage(file="background.png")
-        self.onButtonImage = PhotoImage(file = "onimage.png")
-        self.exitButtonImage = PhotoImage(file = "exitimage.png")
-        label = Label(root, image=self.image,bg = 'white')
-        onButton = Button(root, image = self.onButtonImage)
-        exitButton = Button(root, image = self.exitButtonImage)
+        self.onButtonImage = PhotoImage(file="onimage.png")
+        self.exitButtonImage = PhotoImage(file="exitimage.png")
+        label = Label(root, image=self.image, bg='white')
+        onButton = Button(root, image=self.onButtonImage)
+        exitButton = Button(root, image=self.exitButtonImage, command = self.exit)
         label.pack(side=TOP)
         onButton.pack(side=TOP)
-        exitButton.pack(side = BOTTOM)
+        exitButton.pack(side=BOTTOM)
+
+    def exit(self):
+        exit()
