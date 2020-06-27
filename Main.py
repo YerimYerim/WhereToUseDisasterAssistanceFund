1ca00d859768ff393163dece4660986a383bb744
YerimYerim <vigvang_bip@naver.com>
2020-06-28 01:46:49 +0900

    add introScene.py

--- Main.py
+++ Main.py
@@ -2,10 +2,12 @@ from WhereToUseDisasterAssistanceFund.ClassMain import Main
 from tkinter import *
 
 # ﻿GUI창을 생성하고 라벨을 설정한다.
+from WhereToUseDisasterAssistanceFund.introScene import introScene
 
 root = Tk()
 root.title("경기 지역화폐가맹점 검색 어플리케이션")
-root.geometry("800x800+100+100")
+root.geometry("1000x800+100+100")
 root.resizable(TRUE, TRUE)
+root.configure(bg = 'white')
 mainScene = Main(root)
 root.mainloop()
--- introScene.py
+++ introScene.py
@@ -1,6 +1,15 @@
-from tkinter import ttk
+from tkinter import *
 
 
 class introScene:
-    def __init__(self):
-        pass
+    def __init__(self,root):
+
+        self.image = PhotoImage(file="background.png")
+        self.onButtonImage = PhotoImage(file = "onimage.png")
+        self.exitButtonImage = PhotoImage(file = "exitimage.png")
+        label = Label(root, image=self.image,bg = 'white')
+        onButton = Button(root, image = self.onButtonImage)
+        exitButton = Button(root, image = self.exitButtonImage)
+        label.pack(side=TOP)
+        onButton.pack(side=TOP)
+        exitButton.pack(side = BOTTOM)
